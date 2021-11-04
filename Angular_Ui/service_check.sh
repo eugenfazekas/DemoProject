58c121392ef662e404df49bf4c6432655f120ee6
Eugen Fazekas <skybolt83@gmail.com>
2021-11-04 23:14:58 +0200

    added services,deployments,pv,pvc to kubernetes deployment

---
+++ Angular_Ui/service_check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash     
+servicePath=$1     
+serviceName=$2     
+response=noResponse     
+statusOk='{"status":"UP"}'     
+check=false     
+while [[ $check = false ]]     
+do     
+	response="$(curl http://$servicePath/actuator/health)"     
+		if [[ $response = $statusOk ]]     
+		then     
+			check=true     
+			echo Service "$serviceName" is online     
+			break     
+		else     
+			echo Service "$serviceName" is offline     
+		fi     
+		sleep 1     
+done     
